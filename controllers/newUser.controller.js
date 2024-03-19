ad55e04461b13fd761d86e728b68f2f6ad446ad8
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-03-19 21:51:31 +0530

    add salt

--- controllers/newUser.controller.js
+++ controllers/newUser.controller.js
@@ -1,6 +1,25 @@
 const bcrypt = require("bcrypt");
 const newUser = require("../models/newUser.model.js");
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Find the user by email
+    const user = await newUser.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // Compare the provided password with the hashed password stored in the database
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    // If the password is valid, return the user
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 // Register User
 const registerUser = async (req, res) => {
   try {
