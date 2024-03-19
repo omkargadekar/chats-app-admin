2e7e2e1cf0ee6b01221a0549c801480cabd72e22
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-03-19 21:39:27 +0530

    add corss

--- server.js
+++ server.js
@@ -17,7 +17,19 @@ connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3030",
+      "*",
+      "https://chatsapp-nw05.onrender.com",
+      "http://ec2-52-206-76-43.compute-1.amazonaws.com:8000",
+      "https://chats-app-admin.onrender.com",
+    ], // Add other domains as needed
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
@@ -64,20 +76,6 @@ const io = require("socket.io")(server, {
     ],
   },
 });
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3030",
-      "*",
-      "https://chatsapp-nw05.onrender.com",
-      "http://ec2-52-206-76-43.compute-1.amazonaws.com:8000",
-      "https://chats-app-admin.onrender.com",
-    ], // Add other domains as needed
-    credentials: true,
-  })
-);
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
